aa760197aa6972ea976050f45e57a05073ccec24
oc <ckcz123@126.com>
2019-06-07 20:23:24 +0800

    tileset compressor

---
+++ tileset-compressor/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace tileset_compressor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(263, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1. 本工具可以对额外素材进行合并以节省空间。";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(359, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2. 具体而言，会仅把本塔使用到的图块，合并到一张素材图片上，";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(269, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "并替换全塔属性和各个楼层地图数据等进行适配。";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(353, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "3. 被替换的楼层文件备份将放在floors.backup目录下，其他文件";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(197, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "将会生成对应.backup.js进行备份。";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 122);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(347, 12);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "4. 本工具仅应在发布前使用，使用后必须检查编辑器是否正常！";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(147, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "确定，开始压缩";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.textBox1.Location = new System.Drawing.Point(20, 179);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(348, 183);
+            this.textBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 374);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "额外素材合并工具 By 艾之葵";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ tileset-compressor/Form1.cs
@@ -0,0 +1,408 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace tileset_compressor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void clearMsg()
+        {
+            textBox1.Text = "";
+        }
+
+        public void addMsg(string text)
+        {
+            textBox1.AppendText(text + "\r\n");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clearMsg();
+            clean();
+            process();
+            clean();
+        }
+
+        private void clean()
+        {
+            directory = null;
+            data = null;
+            bitmaps.ForEach(x=>
+            {
+                if (x != null) x.Dispose();
+            });
+            tilesets.Clear();
+            floorIds.Clear();
+            map.Clear();
+            bitmaps.Clear();
+            errors.Clear();
+            now = 10000;
+            closeBitmap();
+        }
+
+        private void process()
+        {
+            addMsg("------ 开始处理 ------");
+            // --- Find directory
+            string[] dirs = { ".", "project", "..\\project" };
+            foreach (var dir in dirs)
+            {
+                if (File.Exists(Path.Combine(dir, "data.js"))
+                    && Directory.Exists(Path.Combine(dir, "floors"))
+                    && Directory.Exists(Path.Combine(dir, "images")))
+                {
+                    directory = dir;
+                    break;
+                }
+            }
+            if (directory == null)
+            {
+                addMsg("无法找到floors目录，images目录或data.js文件！");
+                addMsg("------ 处理中断，退出 ------");
+                return;
+            }
+            if (File.Exists(Path.Combine(directory, "data.backup.js")))
+            {
+                addMsg("本塔好像已经合并过了，请删除data.backup.js以确认重新进行合并。");
+                addMsg("------ 处理中断，退出 ------");
+                return;
+            }
+
+            addMsg("processing...");
+            if (!readData()) return;
+            if (!readFloors()) return;
+            if (!writeTileset()) return;
+            if (!writeData()) return;
+            if (!processMaps()) return;
+            addMsg("------ 处理完毕 ------");
+            // errors.ForEach(addMsg);
+            foreach (var error in errors) addMsg(error);
+            addMsg("记得进入游戏和编辑器检查是否正常！");
+        }
+
+        private string directory = null;
+        private string data = null;
+        private List<string> floorIds = new List<string>();
+        private List<string> tilesets = new List<string>();
+        private Dictionary<int, int> map = new Dictionary<int, int>();
+        private List<Bitmap> bitmaps = new List<Bitmap>();
+        private HashSet<string> errors = new HashSet<string>();
+        private int now = 10000;
+
+        private bool readData()
+        {
+            addMsg("正在读取data.js文件......");
+            data = string.Join("\r\n", File.ReadAllLines(Path.Combine(directory, "data.js")).Skip(1));
+            try
+            {
+                JObject info = (JObject) JsonConvert.DeserializeObject(data);
+                floorIds = ((JArray)info["main"]["floorIds"]).Values<string>().ToList();
+                addMsg("本塔使用到的楼层ID： " + string.Join(", ", floorIds));
+                tilesets = ((JArray) info["main"]["tilesets"]).Values<string>().ToList();
+                addMsg("本塔使用到的tilesets素材： "+string.Join(", ", tilesets));
+                addMsg("成功读取data.js文件！");
+                return true;
+            }
+            catch (Exception e)
+            {
+                addMsg("ERROR: 无法处理data.js文件。");
+                addMsg(e.ToString());
+                addMsg("------ 处理中断，退出 ------");
+                return false;
+            }
+        }
+
+        private bool writeData()
+        {
+            addMsg("正在写入data.js文件......");
+            try
+            {
+                File.Copy(Path.Combine(directory, "data.js"), Path.Combine(directory, "data.backup.js"), true);
+                addMsg("备份 data.js -> data.backup.js");
+                JObject info = (JObject)JsonConvert.DeserializeObject(data);
+                JArray array = (JArray)info["main"]["tilesets"];
+                if (bitmaps.Any())
+                {
+                    array.ReplaceAll("tilesets.min.png");
+                    addMsg("替换tilesets为 tilesets.min.png");
+                }
+                else
+                {
+                    array.RemoveAll();
+                    addMsg("移除tilesets项");
+                }
+                File.WriteAllText(Path.Combine(directory, "data.js"),
+                    "var data_a1e2fb4a_e986_4524_b0da_9b7ba7c0874d = \r\n" + JsonConvert.SerializeObject(info, Formatting.Indented));
+                addMsg("成功写入data.js文件！");
+                return true;
+            }
+            catch (Exception e)
+            {
+                addMsg("ERROR: 无法写入data.js文件。");
+                addMsg(e.ToString());
+                addMsg("------ 处理中断，退出 ------");
+                return false;
+            }
+        }
+
+        private bool readFloors()
+        {
+            addMsg("正在处理楼层文件......");
+            // string[] files = Directory.GetFiles(Path.Combine(directory, "floors"));
+            Directory.CreateDirectory(Path.Combine(directory, "floors.backup"));
+            foreach (var floorId in floorIds)
+            {
+                var filename = floorId + ".js";
+                var path = Path.Combine(directory, "floors", filename);
+                addMsg("正在处理 "+filename+"...");
+
+                // --- 替换
+                bool modified = false;
+                try
+                {
+                    string content = string.Join("\r\n", File.ReadAllLines(path).Skip(1));
+                    JObject info = (JObject) JsonConvert.DeserializeObject(content);
+                    // 获得map, bgmap, fgmap
+                    foreach (var s in new[] {"map", "bgmap", "fgmap"})
+                    {
+                        if (info[s] != null)
+                        {
+                            JArray array = (JArray) info[s];
+
+                            JArray another = new JArray();
+
+                            foreach (var child in array.Children())
+                            {
+                                JArray line = new JArray();
+                                foreach (var i in child.Children())
+                                {
+                                    var v = i.ToObject<int>();
+                                    if (v >= 10000)
+                                    {
+                                        modified = true;
+                                        if (!map.ContainsKey(v))
+                                        {
+                                            addMsg("图块映射： "+v+" -> "+now);
+                                            map[v] = now++;
+                                            bitmaps.Add(getBitmap(v));
+                                        }
+                                        v = map[v];
+                                    }
+                                    line.Add(new JValue(v));
+                                }
+                                another.Add(line);
+                            }
+                            array.Replace(another);
+                        }
+                    }
+                    if (modified)
+                    {
+                        File.Copy(path, Path.Combine(directory, "floors.backup", filename), true);
+                        File.WriteAllText(path, "main.floors." + floorId + "=\r\n" + JsonConvert.SerializeObject(info, Formatting.Indented));
+                        addMsg("备份 " + filename + " -> floors.backup，并写入文件... 成功");
+                    }
+                    else
+                    {
+                        addMsg(filename+"中不含有额外素材。");
+                    }
+                }
+                catch (Exception e)
+                {
+                    addMsg("ERROR: 无法处理楼层文件" + filename + "。");
+                    addMsg(e.ToString());
+                    addMsg("------ 处理中断，退出 ------");
+                    return false;
+                }
+            }
+            addMsg("楼层文件处理完毕！");
+            return true;
+        }
+
+        private Bitmap getBitmap(int number)
+        {
+            int id = number / 10000 - 1, index = number % 10000;
+            if (id < 0 || id >= tilesets.Count)
+            {
+                var msg = "无法找到第" + id + "个额外素材，使用透明块代替";
+                addMsg(msg);
+                errors.Add(msg);
+                return null;
+            }
+            openBitmap(tilesets[id]);
+            if (tileset == null)
+            {
+                var msg = "无法打开额外素材" + tilesets[id] + "，使用透明块代替";
+                addMsg(msg);
+                errors.Add(msg);
+                return null;
+            }
+            int width = tileset.Width, w = width / 32, h = tileset.Height / 32;
+            int row = index / w, col = index % w;
+            if (row >= h)
+            {
+                var msg = "素材" + number + "不在" + tilesets[id] + "上，使用透明块代替";
+                addMsg(msg);
+                errors.Add(msg);
+                return null;
+            }
+            Bitmap bitmap = new Bitmap(32, 32);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            graphics.DrawImage(tileset, new Rectangle(0, 0, 32, 32), col * 32, row * 32, 32, 32, GraphicsUnit.Pixel);
+            graphics.Dispose();
+            return bitmap;
+            // return tileset.Clone(new Rectangle(row * 32, col * 32, 32, 32), tileset.PixelFormat);
+        }
+
+        private Bitmap tileset = null;
+        private string tilesetName = null;
+        private void openBitmap(string name)
+        {
+            if (tilesetName == name) return;
+            closeBitmap();
+            tilesetName = name;
+            addMsg("正在打开素材文件"+tilesetName+"...");
+            if (!File.Exists(Path.Combine(directory, "images", tilesetName)))
+            {
+                addMsg("ERROR: 无法打开文件" + tilesetName);
+                return;
+            }
+            tileset = (Bitmap) Image.FromFile(Path.Combine(directory, "images", tilesetName));
+        }
+
+        private void closeBitmap()
+        {
+            if (tileset != null)
+            {
+                tileset.Dispose();
+                tileset = null;
+            }
+            tilesetName = null;
+        }
+
+        private bool writeTileset()
+        {
+            closeBitmap();
+            if (!bitmaps.Any())
+            {
+                addMsg("没有使用过的额外素材，不写入素材文件...");
+                return true;
+            }
+            addMsg("开始写入tilesets.min.png...");
+            var count = bitmaps.Count;
+            int width = ((count - 1) / 1000 + 1) * 10, height = (count - 1) / width + 1;
+            addMsg("素材总数："+count+"，构建"+width+"*"+height+"的素材。");
+            var bitmap = new Bitmap(32 * width, 32 * height);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            for (var i = 0; i < count; ++i)
+            {
+                var now = bitmaps[i];
+                if (now == null) continue;
+                graphics.DrawImage(now, new Point(32 * (i%width), 32 * (i/width)));
+            }
+            try
+            {
+                var path = Path.Combine(directory, "images", "tilesets.min.png");
+                File.Delete(path);
+                bitmap.Save(path, ImageFormat.Png);
+                graphics.Dispose();
+                bitmap.Dispose();
+                addMsg("成功生成并写入tilesets.min.png！");
+                return true;
+            }
+            catch (Exception e)
+            {
+                graphics.Dispose();
+                bitmap.Dispose();
+                addMsg("ERROR: 无法将tilesets.min.png写入到文件...");
+                addMsg(e.ToString());
+                addMsg("------ 处理中断，退出 ------");
+                return false;
+                
+            }
+        }
+
+        private bool processMaps()
+        {
+            addMsg("读取并处理maps.js...");
+            if (!File.Exists(Path.Combine(directory, "maps.js")))
+            {
+                addMsg("maps.js不存在，无需处理...");
+                return true;
+            }
+            var content = string.Join("\r\n", File.ReadAllLines(Path.Combine(directory, "maps.js")).Skip(1));
+            try
+            {
+                bool modified = false;
+                JObject info = (JObject)JsonConvert.DeserializeObject(content);
+                JObject another = new JObject();
+                var enumerator = info.GetEnumerator();
+                while (enumerator.MoveNext())
+                {
+                    var key = enumerator.Current.Key;
+                    var value = enumerator.Current.Value;
+                    try
+                    {
+                        int index = Convert.ToInt32(key);
+                        if (index >= 10000)
+                        {
+                            if (map.ContainsKey(index))
+                            {
+                                modified = true;
+                                var mapped = Convert.ToString(map[index]);
+                                var cloned = value.DeepClone();
+                                cloned["id"] = new JValue("X" + mapped);
+                                another.Add(mapped, cloned);
+                                continue;
+                            }
+                            addMsg("WARNING：maps.js中的"+index+"没有被映射！");
+                        }
+                        another.Add(key, value.DeepClone());
+                    }
+                    catch (Exception)
+                    {
+                        addMsg("无法识别maps.js中的"+key+"... 忽略");
+                        another.Add(key, value.DeepClone());
+                    }
+                }
+                enumerator.Dispose();
+
+                if (modified)
+                {
+                    addMsg("备份maps.js -> maps.backup.js并写入... 成功");
+                    File.Copy(Path.Combine(directory, "maps.js"), Path.Combine(directory, "maps.backup.js"), true);
+                    File.WriteAllText(Path.Combine(directory, "maps.js"),
+                        "var maps_90f36752_8815_4be8_b32b_d7fad1d0542e = \r\n" +
+                        JsonConvert.SerializeObject(another, Formatting.Indented));
+                }
+                else
+                {
+                    addMsg("maps.js没有被修改");                    
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                addMsg("ERROR: 无法处理maps.js文件。");
+                addMsg(e.ToString());
+                addMsg("------ 处理中断，退出 ------");
+                return false;
+            }
+        }
+    }
+}
